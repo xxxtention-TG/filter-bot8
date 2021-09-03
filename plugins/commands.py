726ae75091fd13d532264962a2a5e420f2d071c9
XXXTENTACION <88360151+xxxtention-TG@users.noreply.github.com>
2021-09-03 19:11:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -243,12 +243,12 @@ async def help(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("🕵‍♂️ CREATOR", url="https://t.me/Lucifer8880"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("Channel", url="https://t.me/MOVIESWORLD54"),
+                    InlineKeyboardButton("Group", url="https://t.me/MOVIESWORLD52")
                 ]
             ]
         ),
