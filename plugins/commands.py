ad20643c5993c0511db40630bce3453896ea7396
XXXTENTACION <88360151+xxxtention-TG@users.noreply.github.com>
2021-09-03 19:01:28 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -217,7 +217,7 @@ async def start(client, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("Command Help", callback_data="help_data")
+                    InlineKeyboardButton('Group', url="https://t.me/MOVIESWORLD52")
                 ]
             ]
         ),
